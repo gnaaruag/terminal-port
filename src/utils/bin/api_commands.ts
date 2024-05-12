95be50cae71604ff41520a754757d9506ed3cfa8
Gaurang Ratnaparkhi <68043860+gnaaruag@users.noreply.github.com>
2024-05-12 13:31:55 +0530

    fix build error

--- src/utils/bin/api_commands.ts
+++ src/utils/bin/api_commands.ts
@@ -1,7 +1,5 @@
 // // List of commands that require API calls
 
-import { getProjects } from '../api';
-import { getQuote } from '../api';
 import { getReadme } from '../api';
 import { getWeather } from '../api';
 
